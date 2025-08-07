1590f9ba9242101a13ffce8a18c41db5e36b17d2
Pedro Matos <pedrogabriel_r@hotmail.com>
2025-08-07 14:22:42 -0300

    Corrige mensagem de saída do servidor para exibir caracteres corretos

--- ChatRMIDistribuido/src/servidor/ServidorChatApp.java
+++ ChatRMIDistribuido/src/servidor/ServidorChatApp.java
@@ -3,6 +3,8 @@ package servidor;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
+import servidor.ServidorChat;
+
 // Aplicação principal para iniciar o servidor de chat
 public class ServidorChatApp {
     public static void main(String[] args) {
@@ -10,7 +12,7 @@ public class ServidorChatApp {
             ServidorChat servidor = new ServidorChat();
             Registry registro = LocateRegistry.createRegistry(1099);
             registro.rebind("ServidorChat", servidor);
-            System.out.println("Servidor RMI pronto para receber conex�es!!!!");
+            System.out.println("Servidor RMI pronto para receber conexões!!!!");
         } catch (Exception e) {
             System.err.println("Erro ao iniciar o servidor: " + e.getMessage());
             e.printStackTrace();
